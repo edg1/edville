de596ee65445639208898dc7d404c2b36f879608
unknown <alexjin9317@outlook.com>
2015-02-05 21:30:24 +0800

    committing changes + added dist folder

--- sites/all/modules/contrib/rooms/modules/rooms_booking_manager/js/rooms_sticky_booking_legend.js
+++ sites/all/modules/contrib/rooms/modules/rooms_booking_manager/js/rooms_sticky_booking_legend.js
@@ -8,21 +8,23 @@
 Drupal.behaviors.rooms_sticky_booking_legend = {
   attach: function(context) {
 
-    if ($('.booking-legend').offset() !== null) {
-      var top = $('.booking-legend').offset().top - parseFloat($('.booking-legend').css('marginTop').replace(/auto/, 0));
-      $(window).scroll(function (event) {
-        // How far has the user scrolled vertically.
-        var y = $(this).scrollTop();
+    if ($('.booking-legend').offset() != null){
+      if ($('.booking-legend').offset() !== null) {
+        var top = $('.booking-legend').offset().top - parseFloat($('.booking-legend').css('marginTop').replace(/auto/, 0));
+        $(window).scroll(function (event) {
+          // How far has the user scrolled vertically.
+          var y = $(this).scrollTop();
 
-        // When the user scrolls past the top of the booking legend.
-        if (y >= top) {
-          // Fix the legend in place on the page.
-          $('.booking-legend').addClass('is-fixed');
-        } else {
-          // Otherwise un-fix it.
-          $('.booking-legend').removeClass('is-fixed');
-        }
-      });
+          // When the user scrolls past the top of the booking legend.
+          if (y >= top) {
+            // Fix the legend in place on the page.
+            $('.booking-legend').addClass('is-fixed');
+          } else {
+            // Otherwise un-fix it.
+            $('.booking-legend').removeClass('is-fixed');
+          }
+        });
+      }
     }
   }
 };
